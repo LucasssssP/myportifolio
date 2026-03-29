52e2402a3da04d512cfb913ce072bfcc6f485948
LucasDEV <lucassantospacheco2000@gmail.com>
2026-03-29 17:55:03 -0300

    Commit 1 Site Portifolio v1

---
+++ script.js
@@ -0,0 +1,38 @@
+function falarComigo() {
+  window.open("https://wa.me/5513991652453");
+}
+
+function enviar() {
+  let nome = document.getElementById("nome").value;
+  let mensagem = document.getElementById("mensagem").value;
+  let telefone = document.getElementById("telefone").value;
+
+  // remove tudo que não é número
+telefone = telefone.replace(/\D/g, "");
+
+
+  if (nome === "" || mensagem === "" || telefone === "") {
+    alert("Preencha todos os campos!");
+    return;
+  }
+  // adiciona o código do Brasil
+telefone = "55" + telefone;
+  let texto = `Olá, me chamo ${nome}. ${mensagem}. Retorne no número ${telefone}`;
+  let url = "https://wa.me/5513991652453?text=" + encodeURIComponent(texto);
+
+  window.open(url);
+}
+
+function formatarTelefone(input) {
+  let valor = input.value.replace(/\D/g, "");
+
+  if (valor.length > 11) valor = valor.slice(0, 11);
+
+  if (valor.length > 6) {
+    input.value = `(${valor.slice(0,2)}) ${valor.slice(2,7)}-${valor.slice(7)}`;
+  } else if (valor.length > 2) {
+    input.value = `(${valor.slice(0,2)}) ${valor.slice(2)}`;
+  } else {
+    input.value = valor;
+  }
+}
